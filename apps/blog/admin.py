dfc7ecd3d81e7a8d3ea4b3f0cd1288788bbf42d5
DeFidelity <adeyemihaki@gmail.com>
2021-12-17 09:39:13 +0100

    wagtail reconfigurations

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import BlogPageTag, BlogPage, BlogPageGalleryImage
 
-# Register your models here.
+
+
+
+class BlogPageDisplay(admin.ModelAdmin):
+    list_display = ('title','intro','tags','date')
+
+admin.site.register(BlogPage,BlogPageDisplay)
+admin.site.register(BlogPageTag)
+admin.site.register(BlogPageGalleryImage)
--- apps/blog/models.py
+++ apps/blog/models.py
@@ -1,21 +1,130 @@
 from django.db import models
+from django.utils import timezone
+from django import forms
+from apps.accounts.models import User
 
-from wagtail.core.models import Page
+
+
+from wagtail.snippets.models import register_snippet
+from wagtail.core.models import Page, Orderable
 from wagtail.core.fields import RichTextField
-from wagtail.admin.edit_handlers import FieldPanel
+from modelcluster.fields import ParentalKey, ParentalManyToManyField
+from wagtail.core.fields import RichTextField
+from wagtail.images.edit_handlers import ImageChooserPanel
+from wagtail.search import index
+from modelcluster.contrib.taggit import ClusterTaggableManager
+from taggit.models import TaggedItemBase
+from wagtail.admin.edit_handlers import FieldPanel, InlinePanel, MultiFieldPanel
 
 
-class HomePage(Page):
+
+
+# class HomePage(Page):
+#     body = RichTextField(blank=True)
+#
+#     content_panels = Page.content_panels + [
+#         FieldPanel('body', classname="full"),
+#     ]
+#
+
+class BlogIndexPage(Page):
     body = RichTextField(blank=True)
+    intro = RichTextField(blank=True)
 
     content_panels = Page.content_panels + [
+        FieldPanel('intro', classname="full"),
         FieldPanel('body', classname="full"),
     ]
 
+@register_snippet
+class BlogCategory(models.Model):
+    name = models.CharField(max_length=255)
+    icon = models.ForeignKey(
+        'wagtailimages.Image', null=True, blank=True,
+        on_delete=models.SET_NULL, related_name='+'
+    )
+
+    panels = [
+        FieldPanel('name'),
+        ImageChooserPanel('icon'),
+    ]
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'blog categories'
 
 
 
+class BlogTagIndexPage(Page):
 
+    def get_context(self, request):
+
+        # Filter by tag
+        tag = request.GET.get('tag')
+        blogpages = BlogPage.objects.filter(tags__name=tag)
+
+        # Update template context
+        context = super().get_context(request)
+        context['blogpages'] = blogpages
+        return context
+
+
+
+class BlogPageTag(TaggedItemBase):
+    content_object = ParentalKey(
+        'BlogPage',
+        related_name='tagged_items',
+        on_delete=models.CASCADE
+    )
+
+
+
+class BlogPage(Page):
+    author = models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
+    intro = models.CharField(max_length=250)
+    body = RichTextField(blank=True)
+    tags = ClusterTaggableManager(through=BlogPageTag, blank=True)
+    categories = ParentalManyToManyField(BlogCategory, blank=True)
+    date = models.DateField("Post date",default=timezone.now)
+
+    def main_image(self):
+        gallery_item = self.gallery_images.first()
+        if gallery_item:
+            return gallery_item.image
+        else:
+            return None
+
+
+    search_fields = Page.search_fields + [
+        index.SearchField('intro'),
+        index.SearchField('body'),
+    ]
+
+    content_panels = Page.content_panels + [
+        MultiFieldPanel([
+            FieldPanel('author'),
+            FieldPanel('date'),
+            FieldPanel('tags'),
+            FieldPanel('categories', widget=forms.CheckboxSelectMultiple),
+        ], heading="Blog information"),
+        FieldPanel('intro'),
+        FieldPanel('body'),
+        InlinePanel('gallery_images', label="Gallery images"),
+    ]
+
+class BlogPageGalleryImage(Orderable):
+    page = ParentalKey(BlogPage, on_delete=models.CASCADE, related_name='gallery_images')
+    image = models.ForeignKey(
+        'wagtailimages.Image', on_delete=models.CASCADE, related_name='+'
+    )
+    caption = models.CharField(blank=True, max_length=250)
+
+    panels = [
+        ImageChooserPanel('image'),
+        FieldPanel('caption'),
+    ]
 
 
 
