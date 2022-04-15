6006972ef509b085d6bef2a3c63743da41831150
DeFidelity <adeyemihaki@gmail.com>
2022-04-15 23:02:59 +0100

    s3 config start

--- iraf/settings.py
+++ iraf/settings.py
@@ -195,3 +195,12 @@ SOCIALACCOUNT_QUERY_EMAIL = True
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# S3 configurationDEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+AWS_ACCESS_KEY_ID =
+AWS_SECRET_ACCESS_KEY =
+AWS_STORAGE_BUCKET_NAME = 'heedngrow'
+AWS_QUERYSTRING_AUTH = False
